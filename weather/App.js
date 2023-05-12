7fe375c4edec0ed525f1b228b8bb2bc7099a2e28
그램 <darkzhon@hanmail.net>
2023-05-12 15:55:42 +0900

    날짜앱을 위한 프론트 디자인

--- weather/App.js
+++ weather/App.js
@@ -3,26 +3,50 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex:1 }}>
-      <View style={{ flex:1, backgroundColor:"teal" }}></View>
-      <View style={{ flex:5, flexDirection:'row'}}>
-        <View style={{ flex:1, backgroundColor:"red"}}></View>
-        <View style={{ flex:4, backgroundColor:"black"}}></View>
-        <View style={{ flex:1, backgroundColor:"blue"}}></View>
+    <View style={styles.container}>
+      <StatusBar style='dark' />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
       </View>
-      <View style={{ flex:1, backgroundColor:"orange" }}></View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+      </View>
     </View>
   );
 }200
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1, 
+    backgroundColor:"orange"
   },
-  text:{
-    fontSize:28,
-  }
+  city:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  cityName:{
+    color:"black",
+    fontSize:68,
+    fontWeight:"500"
+  },
+  weather:{
+    flex:3,
+  },
+  day:{
+    flex:1,
+    alignItems:"center",
+  },
+  temp:{
+    marginTop:50,
+    fontSize:178,
+  },
+  description:{
+    marginTop:-30,
+    fontSize:60,
+  },
+
 });
