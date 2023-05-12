37133872bc118b1a78cb40ee89fa57106a0af97c
그램 <darkzhon@hanmail.net>
2023-05-12 15:28:16 +0900

    레이아웃 시스템 실습

--- weather/App.js
+++ weather/App.js
@@ -3,12 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello i made RN app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex:1 }}>
+      <View style={{ flex:1, backgroundColor:"teal" }}></View>
+      <View style={{ flex:5, flexDirection:'row'}}>
+        <View style={{ flex:1, backgroundColor:"red"}}></View>
+        <View style={{ flex:4, backgroundColor:"black"}}></View>
+        <View style={{ flex:1, backgroundColor:"blue"}}></View>
+      </View>
+      <View style={{ flex:1, backgroundColor:"orange" }}></View>
     </View>
   );
-}
+}200
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +22,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:28,
+  }
 });
