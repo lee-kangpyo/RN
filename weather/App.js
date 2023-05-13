ffa0bbfdb37fd6a05ac454c9df8fb874b99bdeab
그램 <darkzhon@hanmail.net>
2023-05-14 02:25:14 +0900

    위치 정보를 알기위해 expo-location 사용하기

--- weather/App.js
+++ weather/App.js
@@ -1,14 +1,38 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 
 const { width:SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [posName, setPosName] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setOk(false);
+      return;
+    }
+    let {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+    setLocation({latitude, longitude});
+
+    const location = await Location.reverseGeocodeAsync(location, {useGoogleMaps:false});
+    console.log(location);
+    (location[0].city)?setPosName(location[0].city):setPosName(location[0].district);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style='dark' />
       <View style={styles.city}>
-        <Text style={styles.cityName}>서울</Text>
+        <Text style={styles.cityName}>{posName}</Text>
       </View>
       <ScrollView 
         pagingEnabled
