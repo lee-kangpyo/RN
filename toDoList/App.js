f0498f08237836d16c650914bab162c5e33942f0
그램 <darkzhon@hanmail.net>
2023-05-16 15:27:14 +0900

    투두 리스트

--- toDoList/App.js
+++ toDoList/App.js
@@ -1,15 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
 import { theme } from './color';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Fontisto } from '@expo/vector-icons'; 
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
+  const [tap, setTap] = useState(1);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({})
+  const travel = () => setTap(2);
+  const work = () => setTap(1);
+  const onChangeText = (payLoad) => setText(payLoad);
+  const saveToDos = async (todos) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    if (s) setToDos(JSON.parse(s));
+  }
+  useEffect(() => {
+    loadToDos()
+  }, []);
+  const addToDo = async () => {
+    if(text === "") return;
+    //const newToDos = Object.assign({}, toDos, {[Date.now()]:{text, sep:tap}}) 2가지 방식이있음.
+    const newToDos = {...toDos, [Date.now()]:{text, sep:tap}};
+    console.log(newToDos)
+    await saveToDos(newToDos)
+    setToDos(newToDos);
+    setText("");
+  }
+  const deleteTodo = async (key) => {
+    Alert.alert(
+        "삭제", 
+        "제거 하시겠습니까?",
+        [
+          {text:"취소"},
+          {text:"확인", onPress: async () => {
+            const newToDos = {...toDos}
+            delete newToDos[key]
+            await saveToDos(newToDos)
+            setToDos(newToDos);
+          }}
+        ]
+      )
+    
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <Text style={styles.btnText}>Work</Text>
-        <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color:(tap === 1)?"white":theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color:(tap === 2)?"white":theme.grey}}>Travel</Text>
+        </TouchableOpacity>
       </View>
+      <TextInput 
+        value={text}
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        placeholder={(tap === 1)?"할 일을 추가하세요":"어디에 가고 싶습니까?"} 
+        returnKeyType='done'
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key, idx) => 
+          (toDos[key].sep === tap)?
+            <View key={idx} style={styles.toDo}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteTodo(key)}>
+                <Fontisto name="trash" size={16} color="red" />
+              </TouchableOpacity>
+            </View>
+          :
+            null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -29,6 +101,28 @@ const styles = StyleSheet.create({
     color:"white",
     fontWeight: 600,
     fontSize: 35,
-  }
-
+  },
+  input:{
+    backgroundColor:"white",
+    paddingVertical:15,
+    paddingHorizontal:20,
+    borderRadius:30,
+    marginTop:20,
+    fontSize:16,
+    marginBottom:20
+  },
+  toDo:{
+    backgroundColor:theme.grey,
+    marginBottom: 10,
+    paddingVertical:20,
+    paddingHorizontal:20,
+    borderRadius: 15,
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  toDoText:{
+    color:"white",
+    fontSize:16,
+    fontWeight:"500"
+  },  
 });
--- toDoList/color.js
+++ toDoList/color.js
@@ -1,4 +1,6 @@
 export const theme = {
     bg:"black",
+    white:"#fff",
     grey:"#3A3D40",
+    toDoBg:"#5C5C60"
 }
