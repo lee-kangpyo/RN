7c537f2917829e38d8694ea2f16ec7a771c75ce4
그램 <darkzhon@hanmail.net>
2023-05-27 22:27:59 +0900

    회원가입에 밸리데이션 적용 및 db 저장 전 까지 적용

--- alba/src/screen/SignInScreen.js
+++ alba/src/screen/SignInScreen.js
@@ -1,18 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, StyleSheet, Dimensions, TouchableWithoutFeedback, TextInput } from "react-native";
+import { Text, View, StyleSheet, Dimensions, TouchableWithoutFeedback, TouchableOpacity, TextInput } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { theme } from '../util/color';
 
-import React, {useState} from 'react';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
+
+import React, {useState, useEffect} from 'react';
+import { setUserInfo } from '../../redux/slices/login';
+
+import CustomBtn from '../components/CustomBtn';
 
 const windowWidth = Dimensions.get('window').width;
 
 export default function SignInScreen({navigation}) {
-    const[userType, setUserType] = useState(0)
-    const onClickSeq = (sep) => {
-        setUserType(sep)
+
+    const [step, setStep] = useState(1)
+    const [userInfo, setUserInfo] = useState({});
+    const updateState = (step, inputObj) => {
+        setStep(step);
+        setUserInfo({...userInfo, ...inputObj});
     }
+    
+    //첫번째 page
+        //헤더
+    useEffect(()=>{
+        navigation.setOptions({title:`회원가입 (${step} / 3)`})
+    }, [navigation, step])
+
     return(
-        <View style={styles.container}>
+        <>
+        <TouchableOpacity onPress={()=>console.log(userInfo)}>
+            <Text>useInfo</Text>
+        </TouchableOpacity>
+        {
+            step == 1?
+                <Step1 updateState={updateState} />
+            :step == 2?
+                <Step2 updateState={updateState} />
+            :
+                <>
+                <Text>가입을 환영합니다 고객님</Text>
+                <TouchableOpacity onPress={()=>navigation.navigate('Login')}>
+                    <Text>로그인 하러가기</Text>    
+                </TouchableOpacity>
+                </>
+        
+        }
+        
+        
+        </>
+    );
+}
+const Step1 = ({updateState}) => {
+    const[userType, setUserType] = useState(0)
+
+    const validationSchema = yup.object().shape({
+        userName: yup.string().required('이름을 입력해주세요.'),
+        hpNo: yup.string().required("휴대폰 번호를 입력해주세요").min(10, "휴대폰 번호는 10-11 자리입니다.").max(11, "휴대폰 번호는 10-11 자리입니다."),
+      });
+    
+      const handleFormSubmit = (values) => {
+        // 유효성 검사 통과 후 실행되는 함수
+        updateState(2, {...values, userType:userType})
+      };
+
+    return(
+        <View style={{flex:1, backgroundColor:"white", alignItems:"center"}}>
             <StatusBar style="auto" />
             <View style={styles.sep}>
                 <TouchableWithoutFeedback onPress={()=>setUserType(0)}>
@@ -22,30 +77,227 @@ export default function SignInScreen({navigation}) {
                     <Text style={[styles.sepTxt, userType === 1 && styles.sepSelected]}>알바님</Text>
                 </TouchableWithoutFeedback>
             </View>
-            <InputEl label="이메일" placeholder={"이메일 입력"} keyboardType='email-address'/>
-            
-            <Text>여기는 회원 가입 페이지</Text>
+            <KeyboardAwareScrollView contentContainerStyle={styles.container} resetScrollToCoords={{ x: 0, y: 0 }} scrollEnabled={true}>
+                <Formik
+                    initialValues={{ userName: '', hpNo: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleFormSubmit}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+                        <>
+                        <InputEl 
+                            label="이름" 
+                            placeholder={"이름"} 
+                            onChangeText={handleChange('userName')}
+                            onBlur={handleBlur('userName')}
+                            value={values.userName}
+                            errorMsg={errors.userName} 
+                        />
+                        <InputEl 
+                            label="전화 번호" 
+                            placeholder={"전화번호"} 
+                            keyboardType='number-pad'
+                            onChangeText={handleChange('hpNo')}
+                            onBlur={handleBlur('hpNo')}
+                            value={values.hpNo}
+                            errorMsg={errors.hpNo}
+                        />
+                        <View style={styles.viewContainer}>
+                            <CustomBtn txt="다음" onPress={handleSubmit} style={styles.next} color='white'/>
+                        </View>
+                        </>
+                    )}
+
+                </Formik>
+            </KeyboardAwareScrollView>
         </View>
-    );
+    )
+
+    
 }
 
-const InputEl = ({label, placeholder, keyboardType="default", onChangeText}) => {
+
+const Step2 = ({ updateState }) => {
+    const validationSchema = yup.object().shape({
+        password: yup.string().required('비밀번호를 입력해주세요.'),
+        confirmPassword: yup.string().oneOf([yup.ref('password'), null], '비밀번호가 일치하지 않습니다.').required('비밀번호 확인을 입력해주세요.'),
+      });
+    
+      const handleFormSubmit = (values) => {
+        // 유효성 검사 통과 후 실행되는 함수
+        updateState(3, values)
+      };
+    return (
+        <View style={{flex:1, backgroundColor:"white", alignItems:"center"}}>
+            <StatusBar style="auto" />
+            <KeyboardAwareScrollView contentContainerStyle={styles.container} resetScrollToCoords={{ x: 0, y: 0 }} scrollEnabled={true}>
+                <Formik
+                    initialValues={{ password: '', confirmPassword: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleFormSubmit}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+                        <>
+                            <InputEl 
+                                label="비밀번호" 
+                                placeholder={"비밀번호"} 
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                                errorMsg={errors.password} 
+                                secure={true}
+                            />
+                            <InputEl 
+                                label="비밀번호 확인" 
+                                placeholder={"비밀번호 확인"} 
+                                onChangeText={handleChange('confirmPassword')}
+                                onBlur={handleBlur('confirmPassword')}
+                                value={values.confirmPassword}
+                                errorMsg={errors.confirmPassword}
+                                secure={true}
+                            />
+                            <View style={styles.viewContainer}>
+                                <CustomBtn txt="가입하기" onPress={handleSubmit} style={styles.next} color='white'/>
+                            </View>
+                        </>
+                    )}
+                </Formik>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
+
+const emailCheck = ({ updateState }) => {
+    
+    const[userType, setUserType] = useState(0)
+    const[isSend, setIsSend] = useState(false);
+
+    const validationSchema = yup.object().shape({
+        email: yup.string()
+            .email('유효한 이메일을 입력해주세요.')
+            .required('이메일을 입력해주세요.')
+            .test('email-unique', '이미 사용 중인 이메일입니다.', async (value) => {
+                try {
+                    // 서버로 이메일 중복 체크 요청을 보내고 응답을 받아옴
+                    //const response = await axios.post('/api/check-email', {
+                    //  email: value,
+                    //});
+            
+                    // 중복된 이메일인 경우 유효성 검사 실패
+                    //if (response.data.isDuplicate) {
+                    //  return false;
+                    //}
+                  } catch (error) {
+                    console.error('이메일 중복 체크 실패:', error);
+                  }
+            
+                return true; // 중복되지 않은 이메일인 경우 유효성 검사 통과
+            }),
+        emailCheckNumber: yup.string().required('인증번호를 입력해주세요.').min(6, '6자리 숫자를 입력해주세요.').max(6, '6자리 숫자를 입력해주세요.'),
+      });
+    
+      const handleFormSubmit = (values) => {
+        updateState(2, {...values, userType:userType})
+      };
+      const sendEmail = () => {
+        console.log("서버에서 이메일 전송 로직 필요")
+        //이메일 전송이 성공했다면
+        setIsSend(true);
+      }
+    return (
+        <View style={{flex:1, backgroundColor:"white", alignItems:"center"}}>
+            <StatusBar style="auto" />
+            <View style={styles.sep}>
+                <TouchableWithoutFeedback onPress={()=>setUserType(0)}>
+                    <Text style={[styles.sepTxt, userType === 0 && styles.sepSelected]}>사장님</Text>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={()=>setUserType(1)}>
+                    <Text style={[styles.sepTxt, userType === 1 && styles.sepSelected]}>알바님</Text>
+                </TouchableWithoutFeedback>
+            </View>
+            <KeyboardAwareScrollView contentContainerStyle={styles.container} resetScrollToCoords={{ x: 0, y: 0 }} scrollEnabled={true}>
+                <Formik
+                    initialValues={{ email: '', emailCheckNumber:'' }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleFormSubmit}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+                        <>
+                            <InputEl 
+                                label="이메일" 
+                                placeholder={"이메일 입력"} 
+                                keyboardType='email-address'
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                                errorMsg={errors.email}                
+                            />
+
+                            {
+                            isSend?
+                            <>
+                                <View style={styles.viewContainer}>
+                                    <TouchableOpacity onPress={sendEmail}>
+                                        <Text style={styles.emailReSend}>인증번호 재전송 (남은 횟수 4회)</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <InputEl 
+                                    label="인증번호 6자리" 
+                                    placeholder={""} 
+                                    keyboardType='number-pad'
+                                    onChangeText={handleChange('emailCheckNumber')}
+                                    onBlur={handleBlur('emailCheckNumber')}
+                                    value={values.emailCheckNumber}
+                                    errorMsg={errors.emailCheckNumber}                
+                                />
+                                <View style={styles.viewContainer}>
+                                    <CustomBtn txt="다음" onPress={(handleSubmit)} style={styles.next} color='white'/>
+                                </View>
+                            </>
+                            :
+                            <View style={styles.viewContainer}>
+                                <TouchableOpacity onPress={sendEmail}>
+                                    <Text style={styles.emailSend} >인증번호 전송</Text>
+                                </TouchableOpacity>
+                                <View style={styles.txt_sm_cont}>
+                                    <Text style={styles.txt_sm}>인증번호 발송에는 시간이 소요되며 하루 최대 5회까지 전송할 수 있습니다.</Text>
+                                    <Text style={styles.txt_sm}>인증번호는 <Text style={{fontWeight:800}}>입력한 이메일 주소</Text>로 발송됩니다. 수신하지 못했다면 스팸함 또는 해당 이메일 서비스의 설정을 확인해주세요</Text>
+                                </View>
+                            </View>
+                            }
+                        </>
+                    )}
+                </Formik>
+                
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
+
+
+const InputEl = ({label, placeholder, keyboardType="default", onChangeText, onBlur, value, errorMsg, secure=false}) => {
     const [isFocused, setIsFocused] = useState(false);
     const handleFocus = () => setIsFocused(true);
-    const handleBlur = () => setIsFocused(false);
+    const handleBlur = () => {
+        setIsFocused(false);
+        onBlur
+    };
 
     return(
-        <View style={styles.inputBox}>
+        <View style={styles.viewContainer}>
             <Text style={styles.labelTxt}>{label}</Text>
             <TextInput
-                style={[styles.input, isFocused && styles.inputFocused]}
+                style={[styles.input, errorMsg && styles.error, !errorMsg && isFocused && styles.inputFocused,]}
                 onChangeText={onChangeText}
-                value={""}
+                value={value}
                 placeholder={placeholder}
                 keyboardType={keyboardType}
                 onFocus={handleFocus}
                 onBlur={handleBlur}
+                returnKeyType='go'
+                secureTextEntry={secure}
             />
+            {errorMsg && <Text style={styles.error}>{errorMsg}</Text>}
         </View>
     )
 }
@@ -54,7 +306,6 @@ const InputEl = ({label, placeholder, keyboardType="default", onChangeText}) => 
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
         backgroundColor:"white",
         alignItems:"center",
         width:windowWidth,
@@ -66,8 +317,6 @@ const styles = StyleSheet.create({
         margin:16,
         padding:5,
         width:"50%",
-        
-        
     },
     sepTxt:{
         textAlign:"center",
@@ -82,10 +331,9 @@ const styles = StyleSheet.create({
         backgroundColor:"#FFFFFF", 
         color:theme.purple
     },
-    inputBox:{width:windowWidth, paddingHorizontal:24, paddingVertical:12,},
+    viewContainer:{width:windowWidth, paddingHorizontal:24, paddingVertical:8,marginBottom:16,},
     input:{
         height: 40,
-        marginBottom:16,
         borderWidth: 1,
         padding: 10,
         borderBottomColor:theme.grey,
@@ -96,6 +344,36 @@ const styles = StyleSheet.create({
     },
     labelTxt:{
         color:theme.grey,
+    },
+    error:{
+        color:theme.error,
+        borderBottomColor:theme.error,
+    },
+    txt_sm:{
+        fontSize:8,
+        color:"#949494",
+    },
+    txt_sm_cont:{
+        marginTop:24,
+    },
+    emailSend:{
+        width:"100%",
+        padding:4,
+        color:theme.primary,
+        textAlign:"center",
+        borderColor:theme.primary,
+        borderWidth:1,
+        borderRadius:5,
+    },
+    emailReSend:{
+        width:"100%",
+        padding:4,
+        textAlign:"center",
+        borderColor:theme.grey,
+        borderWidth:1,
+        borderRadius:5,
+    },
+    next:{
+        backgroundColor:theme.purple,
     }
-
 })
