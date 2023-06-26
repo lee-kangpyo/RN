347ad8eb2daa97dec51bfeb0b3d039d90dd28830
그램 <darkzhon@hanmail.net>
2023-06-26 18:41:00 +0900

    다음 주소 검색에 경도 위도 값 가져기 추가

--- alba/src/components/SearchAddress.js
+++ alba/src/components/SearchAddress.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Postcode from '@actbase/react-daum-postcode';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 export default function SearchAddress({navigation, route}){
-    const getAddressData = (data) => {
+    const url = useSelector((state) => state.config.url);
+    const getAddressData = async (data) => {
         let defaultAddress='';
         let zoneCode = "";
+        let result = {}
         if(data.buildingName==='')
         {
             defaultAddress='';
@@ -16,11 +20,20 @@ export default function SearchAddress({navigation, route}){
         else{
             defaultAddress="("+data.buildingName+")";
         }
-        //console.log(data)
-        route.params.setAdress({address:data.address, zoneCode:data.zonecode})
+        const address = {address:data.address, zoneCode:data.zonecode}
+
+        const params = Object.entries(address).map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`).join("&");
+
+        await axios.get(url+`/api/v1/getLatLon`, params)
+          .then(function (response) {
+               result = {...response.data}
+          }).catch(function (error) {
+               console.log(error)
+          });
+        route.params.setAdress({...result, address:data.address, zoneCode:data.zonecode})
         navigation.goBack();
-        //this.props.navigation.navigate('Drawers',{screen:'Deliver', params:{zonecode:data.zonecode, address:data.address, defaultAddress:defaultAddress}});
     }
+
     return (
         <Postcode
             style={{ width: '100%', height: '100%' }}
