7a402d3a3da6c12ace92ffd04febe36e525a8e43
그램 <darkzhon@hanmail.net>
2023-07-27 20:53:50 +0900

    출퇴근 기록 10분단위로 끊기

--- apiServer/src/route/api.js
+++ apiServer/src/route/api.js
@@ -265,7 +265,30 @@ router.get("/v1/testTaskManager", async (req, res, next) => {
 
 router.get("/v1/checkStoreLocation", async (req, res, next) => {
     console.log("checkStoreLocation")
-    
+
+    const getCurrentTime = () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, "0");
+        const date = String(now.getDate()).padStart(2, "0");
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+        return `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+    };
+
+    // 시간을 올림 또는 버림으로 맞춰주는 함수
+    const getRoundedTime = (timeString, roundUp = true) => {
+        const time = new Date(timeString);
+        const minutes = time.getMinutes();
+        const roundedMinutes = roundUp ? Math.ceil(minutes / 10) * 10 : Math.floor(minutes / 10) * 10;
+        time.setMinutes(roundedMinutes);
+        time.setSeconds(0);
+        const offset = time.getTimezoneOffset() * 60 * 1000;
+        const localTime = new Date(time.getTime() - offset);
+        const localTimeString = localTime.toISOString().replace("T", " ").slice(0, -1);
+        return localTimeString;
+    };   
     const deg2rad = (deg) => {
         return deg * (Math.PI/180);
     }
@@ -327,16 +350,16 @@ router.get("/v1/checkStoreLocation", async (req, res, next) => {
                 const cstCo = jobChk[0].CSTCO;
                 const alba = myAlbaList.filter((alba) => { return alba.CSTCO === cstCo })
                 const meter = distance(lat, lon, alba[0].LAT, alba[0].LON)
-                if(meter > 20){
-                    const result = await execSql(insertJobChk, {userId:id, cstCo:cstCo, day:curday, lat:Math.floor(lat), lon:Math.floor(lon), jobYn:"N", apvYn:apvYn});
+                if(meter > 50){
+                    const result = await execSql(insertJobChk, {userId:id, cstCo:cstCo, day:curday, lat:Math.floor(lat), lon:Math.floor(lon), jobYn:"N", apvYn:apvYn, chkTime:getRoundedTime(getCurrentTime(), false)});
                     console.log(id+"가 퇴근을 완료함"+cstCo)
                 }
             }else{
                 for (const alba of myAlbaList) {
                     if (alba.RTCL === "N") {
                         const meter = distance(lat, lon, alba.LAT, alba.LON)
-                        if(meter < 20){
-                            const result = await execSql(insertJobChk, {userId:id, cstCo:alba.CSTCO, day:curday, lat:Math.floor(lat), lon:Math.floor(lon), jobYn:"Y", apvYn:apvYn});
+                        if(meter < 50){
+                            const result = await execSql(insertJobChk, {userId:id, cstCo:alba.CSTCO, day:curday, lat:Math.floor(lat), lon:Math.floor(lon), jobYn:"Y", apvYn:apvYn, chkTime:getRoundedTime(getCurrentTime(), true)});
                             console.log(id+"가 출근을 완료함"+alba.CSTCO)
                             break;
                         }
