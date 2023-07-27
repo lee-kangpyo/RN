699a563b91a90930f6f162992d308fabbc1f12c4
그램 <darkzhon@hanmail.net>
2023-07-27 20:54:18 +0900

    태스크 매니저 실행 안되는 이슈 픽스

---
+++ alloha/src/components/CommuteTask.js
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import * as Location from 'expo-location';
+import { theme } from '../util/color';
+import * as TaskManager from 'expo-task-manager';
+
+
+const LOCATION_TASK_NAME = 'background-location-task';
+
+export default function CommuteTask(){
+    const [isTaskStart, setTaskStart] = useState(false)
+    useEffect(() => {
+      (async () => {
+        const {granted:forePerm} = await Location.getForegroundPermissionsAsync()
+        const {granted:backPerm} = await Location.getBackgroundPermissionsAsync()
+        const isTaskStarted = await TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME);
+        if(forePerm && backPerm){
+            await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+                timeInterval: 6000,
+                //deferredUpdatesDistance:1,
+                deferredUpdatesInterval: 100,
+                accuracy: Location.Accuracy.BestForNavigation,
+                showsBackgroundLocationIndicator: true,
+                foregroundService: {
+                    notificationTitle: "알로하",
+                    notificationBody: "알로하가 위치 기반으로 출퇴근을 여부를 체크하고 있습니다.",
+                    notificationColor: "#fff",
+                },  
+            });
+        }
+      })();
+    });
+  
+    return (
+        <>
+        {
+            (isTaskStart)?
+                <Text>태스크 매니저가 실행되지 않고 있습니다.</Text>
+            :
+                <Text>자동 출퇴근 기록 동작중....</Text>
+        }
+        </>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"flex-start",
+        alignItems:"center",
+    },
+    paragraph:{
+        fontSize:16,
+        fontWeight:"bold",
+    },
+    sub_txt:{
+        fontSize:13,
+        
+    },
+    stat_txt:{
+        fontSize:13,
+        color:theme.grey
+    },
+    flexLow:{flexDirection:"row"}
+});
--- alloha/src/components/LocationPermissionBack.js
+++ alloha/src/components/LocationPermissionBack.js
@@ -2,43 +2,41 @@ import { StyleSheet, Text, View, Alert } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import * as Location from 'expo-location';
 import { theme } from '../util/color';
+
 import * as TaskManager from 'expo-task-manager';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as SecureStore from 'expo-secure-store';
 import axios from 'axios';
 
 import { URL } from "@env";
-const LOCATION_TASK_NAME = 'background-location-task';
+import Loading from './Loding';
+
+
 
 export default function GetLocationPermission({Grant, Deny}){
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
     const [fore, setFore] = useState("포그라운드 위치권한 요청중...");
     const [back, setBack] = useState("백그라운드 위치권한 요청중...");
-    const [idGrant, setGrant] = useState();
+    const [isLoading, setLoading] = useState(true)
+    const [isGrant, setGrant] = useState();
   
     useEffect(() => {
       (async () => {
+
+        const {granted:forePerm} = await Location.getForegroundPermissionsAsync()
+        const {granted:backPerm} = await Location.getBackgroundPermissionsAsync()
+        console.log(forePerm, backPerm)
+        if(!forePerm || !backPerm || (forePerm && backPerm)){
+            setLoading(false);
+        }
+
+
         const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
-        
         if (foregroundStatus === 'granted') {
             setFore("포그라운드 위치 권한 허용됨.")
             const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
             if (backgroundStatus === 'granted') {
-                setBack("포그라운드 위치 권한 허용됨.")
-                await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-                timeInterval: 6000,
-                //deferredUpdatesDistance:1,
-                deferredUpdatesInterval: 100,
-                accuracy: Location.Accuracy.BestForNavigation,
-                showsBackgroundLocationIndicator: true,
-                foregroundService: {
-                    notificationTitle: "알로하",
-                    notificationBody: "알로하가 위치 기반으로 출퇴근을 여부를 체크하고 있습니다.",
-                    notificationColor: "#fff",
-                },
-                });
-                console.log("내위치 추적 시작")
+                setBack("백그라운드 위치 권한 허용됨.")
+   
                 setGrant(true);
             }else{
                 setBack("백그라운드 위치 권한 거부됨.")
@@ -54,63 +52,37 @@ export default function GetLocationPermission({Grant, Deny}){
     return (
         <>
         {
-            (idGrant)?
-                <Grant/>
+            (isLoading)?
+                <Loading/>
             :
-                <View style={{...styles.container, marginTop:30}}>
-                    <Text style={styles.paragraph}>출 퇴근 체크를 위해 위치 권한 허용이 필요합니다.</Text>
-                    <Text style={{...styles.sub_txt, marginTop:8}}>만약 위치 권한 동의 화면이 뜨지 않는다면,</Text>
-                    <View style={styles.flexLow}>
-                        <Text style={{...styles.sub_txt, fontWeight:"bold"}}>설정&gt;애플리케이션&gt;권한</Text> 
-                        <Text>에서</Text>
+                (isGrant)?
+                    <Grant/>
+                :
+                    <View style={{...styles.container, marginTop:30}}>
+                        <Text style={styles.paragraph}>출 퇴근 체크를 위해 위치 권한 허용이 필요합니다.</Text>
+                        <Text style={{...styles.sub_txt, marginTop:8}}>만약 위치 권한 동의 화면이 뜨지 않는다면,</Text>
+                        <View style={styles.flexLow}>
+                            <Text style={{...styles.sub_txt, fontWeight:"bold"}}>설정&gt;애플리케이션&gt;권한</Text> 
+                            <Text>에서</Text>
+                        </View>
+                        <View style={styles.flexLow}>
+                            <Text style={{...styles.sub_txt, fontWeight:"bold"}}>위치 권한</Text>
+                            <Text>을 </Text>
+                            <Text style={{...styles.sub_txt, fontWeight:"bold"}}>항상 허용</Text>
+                            <Text>으로 설정 해주세요.</Text>
+                        </View>
+                        
+                        <Text style={{...styles.sub_txt, marginBottom:16,}}>항상 허용이 백그라운드 위치 권한입니다.</Text>
+                        <Text style={styles.stat_txt}>{fore}</Text>
+                        <Text style={styles.stat_txt}>{back}</Text>
                     </View>
-                    <View style={styles.flexLow}>
-                        <Text style={{...styles.sub_txt, fontWeight:"bold"}}>위치 권한</Text>
-                        <Text>을 </Text>
-                        <Text style={{...styles.sub_txt, fontWeight:"bold"}}>항상 허용</Text>
-                        <Text>으로 설정 해주세요.</Text>
-                    </View>
-                    
-                    <Text style={{...styles.sub_txt, marginBottom:16,}}>항상 허용이 백그라운드 위치 권한입니다.</Text>
-                    <Text style={styles.stat_txt}>{fore}</Text>
-                    <Text style={styles.stat_txt}>{back}</Text>
-                    
-
-                </View>
         }
         </>
     );
 };
 
-TaskManager.defineTask(LOCATION_TASK_NAME,  async ({ data, error } ) => {
-    const getCurrentTimeWithDate = () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-    const hours = String(currentDate.getHours()).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
-
-    const currentTimeWithDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    return currentTimeWithDate;
-    };
-
-    if (error) {
-    // check `error.message` for more details.
-    return;
-    }
-    if(data){
-    const id = await AsyncStorage.getItem("id")
-    const uid = await SecureStore.getItemAsync("uuid");
-    const { locations } = data;
-    await axios.get(URL+"/api/v1/checkStoreLocation", {params:{id:id, uuid:uid, lat:locations[0].coords.latitude, lon:locations[0].coords.longitude, day:getCurrentTimeWithDate()}})
-    .catch((err)=>{console.log(err)})
-    }
-});
 
 const styles = StyleSheet.create({
-
     container:{
         flex:1,
         justifyContent:"flex-start",
@@ -129,5 +101,4 @@ const styles = StyleSheet.create({
         color:theme.grey
     },
     flexLow:{flexDirection:"row"}
-
 });
