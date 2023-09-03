0faf5409c654e7705531a0e6ec8175fc151d2450
그램 <darkzhon@hanmail.net>
2023-09-03 17:32:34 +0900

    태스크 매니저 업데이트

---
+++ taskManager/src/route/admin.js
@@ -0,0 +1,31 @@
+const express = require('express')
+var router = express.Router();
+
+const { getTraceUserList, getMyuserTrace } = require('../query/admin'); 
+
+const runMyQuery = require('../utils/MySqlQuery');
+
+router.get("/getTraceUserList", async (req, res, next) => {
+    console.log("getTraceUserList")
+    const result = await runMyQuery(getTraceUserList);
+    res.status(200).json({ result:result});
+})
+
+router.get("/getMyusersTrace", async (req, res, next) => {
+    console.log("getMyuserTrace")
+    const result = await runMyQuery(getMyusersTrace);
+    console.log(result)
+    res.status(200).json({ result:result});
+})
+
+router.get("/getMyuserTrace", async (req, res, next) => {
+    console.log("getMyuserTrace")
+    const {userId, uuid} = req.query;
+    const result = await runMyQuery(getMyuserTrace, [userId, uuid]);
+    console.log(result)
+    res.status(200).json({ result:result});
+})
+
+
+module.exports = router;
+
--- taskManager/src/utils/MySqlQuery.js
+++ taskManager/src/utils/MySqlQuery.js
@@ -3,12 +3,23 @@ const mysql = require('../config/mysql');
 function runQuery(sql, params) {
   return new Promise( async (resolve, reject) => {
     let conn = null;
+    
     try {
       conn = await mysql.getConnection();
       await conn.beginTransaction(); //트랜잭션 시작
-      const [ result ] = await conn.query(sql, params);
+      
+      let value;
+
+      if(params){
+        const [ result ] = await conn.query(sql, params);
+        value = result;
+      }else{
+        const [ result ] = await conn.query(sql);
+        value = result;
+      }
+      
       await conn.commit(); //커밋
-      resolve(result);
+      resolve(value);
     } catch (err) {
       if (conn !== null) {
         await conn.rollback(); //롤백
