d57aa246e2f6db3ef71dd74e1cc33cf35d6e4437
그램 <darkzhon@hanmail.net>
2023-09-14 03:34:11 +0900

    푸시 알림을 위한 기능 구현

--- apiServer/src/route/push.js
+++ apiServer/src/route/push.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var router = express.Router();
 const {execSql} = require("../utils/excuteSql");
-const {sendPush}  = require("../utils/expoPush");
+const {sendPush, checkTicket}  = require("../utils/expoPush");
 
 const axios = require('axios');
 
@@ -9,8 +9,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 router.get("/test", async (req, res, next)=>{
-    console.log("ASdf");
-    sendPush(["lt4S62MGZ0sYZx1kzZYf77", "CQezQ8E5qrty6jieuuFFdf"]);
+    console.log("test");
+    //sendPush(["hH-w5iBNoyx5lpqCeRg7sw", "fkqkgnChudm9c2A7_015hL"]);
+    sendPush([ "fkqkgnChudm9c2A7_015hL"]);
+    res.json({status_code:"00", result:"테스트"});
+})
+
+router.get("/testicket", async (req, res, next)=>{
+    console.log("testicket");
+    checkTicket(
+        [ { status: 'ok', id: '90487d8c-1020-411d-b3a7-666b20d85449' } ]
+    );
+    //sendPush([ "fkqkgnChudm9c2A7_015hL"]);
     res.json({status_code:"00", result:"테스트"}); 
 })
 
--- apiServer/src/utils/expoPush.js
+++ apiServer/src/utils/expoPush.js
@@ -21,8 +21,8 @@ async function sendPush(tokens){
         messages.push({
             to: pushToken,
             sound: 'default',
-            title:"title",
-            body: 'This is a test notification',
+            title:"알로하 테스트 푸시메시지",
+            body: '안녕하세요 알로하 푸시 메시지입니다.',
             data: { withSome: 'data' },
         })
     }
@@ -52,8 +52,56 @@ async function sendPush(tokens){
         }
     }
     })();
+
+    
 }
 
 
+async function checkTicket(tickets){
+    let expo = new Expo();
 
-module.exports = {sendPush};
+    let receiptIds = [];
+    for (let ticket of tickets) {
+    // NOTE: Not all tickets have IDs; for example, tickets for notifications
+    // that could not be enqueued will have error information and no receipt ID.
+    if (ticket.id) {
+        receiptIds.push(ticket.id);
+    }
+    }
+
+    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+    (async () => {
+    // Like sending notifications, there are different strategies you could use
+    // to retrieve batches of receipts from the Expo service.
+    for (let chunk of receiptIdChunks) {
+        try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+        console.log(receipts);
+
+        // The receipts specify whether Apple or Google successfully received the
+        // notification and information about an error, if one occurred.
+        for (let receiptId in receipts) {
+            let { status, message, details } = receipts[receiptId];
+            if (status === 'ok') {
+            continue;
+            } else if (status === 'error') {
+            console.error(
+                `There was an error sending a notification: ${message}`
+            );
+            if (details && details.error) {
+                // The error codes are listed in the Expo documentation:
+                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                // You must handle the errors appropriately.
+                console.error(`The error code is ${details.error}`);
+            }
+            }
+        }
+        } catch (error) {
+        console.error(error);
+        }
+    }
+    })();
+}
+
+
+module.exports = {sendPush, checkTicket};
