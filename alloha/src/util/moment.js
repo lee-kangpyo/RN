61001dcc81954411d549608c7dcd14a28ace7b15
그램 <darkzhon@hanmail.net>
2023-10-25 00:01:47 +0900

    날짜 유틸 함수 추가

--- alloha/src/util/moment.js
+++ alloha/src/util/moment.js
@@ -4,6 +4,60 @@ require('moment/locale/ko');
 // 현재 날짜 객체 생성
 const currentDate = moment();
 
+export const getNextWeek = () => {
+    // 현재 주의 시작 날짜 계산
+    const currentWeekStart = currentDate.startOf('week');
+    // 다음 주의 시작 날짜 계산
+    const nextWeekStart = currentWeekStart.clone().add(7, 'days');
+    //console.log(nextWeekStart.format('YYYYMMDD'));
+    //console.log(nextWeekStart.clone().add(6, 'days').format("YYYYMMDD"))
+    console.log(nextWeekStart.format("YYYY-MM-DD"));
+    return nextWeekStart.format("YYYY-MM-DD");
+}
+
+export const movePrevWeek = (week) => {
+    const dateObject = moment(week);
+    const prevWeekStart = dateObject.clone().subtract(7, 'days');
+    return prevWeekStart.format("YYYY-MM-DD");
+}
+
+export const moveNextWeek = (week) => {
+    const dateObject = moment(week);
+    const nextWeekStart = dateObject.clone().add(7, 'days');
+    return nextWeekStart.format("YYYY-MM-DD");
+}
+
+export const getWeekList = (week) => {
+    // 이번 주의 일자를 담을 배열 초기화
+    const dateObject = moment(week);
+    const weekDates = [];
+
+    // 이번 주의 날짜를 배열에 추가
+    for (let i = 0; i < 7; i++) {
+        const date = dateObject.clone().add(i, 'days');
+        
+        //thisWeekDates.push(date.format('YYYY년 MM월 DD일'));
+        weekDates.push(date);
+    }
+
+    return weekDates;
+}
+
+export const getWeekNumber = (dateString) => {
+    // dateString을 moment 객체로 변환
+    const inputDate = moment(dateString);
+
+    // 월과 주차를 추출
+    const month = inputDate.format('MM');
+
+    // 해당 월의 첫째 주부터의 주차를 계산
+    const startOfMonth = moment(inputDate).startOf('month');
+    const weeksSinceStartOfMonth = inputDate.diff(startOfMonth, 'weeks') + 1;
+    console.log({"month":month, "number":weeksSinceStartOfMonth});
+    return {"month":month, "number":weeksSinceStartOfMonth};
+}
+
+
 export const getCurrentWeek = () => {
 
     // 이번 주의 시작 날짜 계산
@@ -21,22 +75,4 @@ export const getCurrentWeek = () => {
     }
 
     return thisWeekDates
-}
-
-export const getWeekNumber = () => {
-    // 현재 연도 가져오기
-    const currentYear = currentDate.year();
-
-    // 현재 월 가져오기 (0부터 시작하므로 +1 해야 함)
-    const currentMonth = currentDate.month() + 1;
-
-    // 현재 날짜 가져오기
-    const currentDay = currentDate.date();
-
-    // 주차 번호 계산
-    const currentWeekNumber = Math.ceil(currentDay / 7);
-
-    // 결과 출력
-    console.log(`${currentMonth}월 ${currentWeekNumber}주차`);
-    return {"month":currentMonth, "num":currentWeekNumber}
 }
