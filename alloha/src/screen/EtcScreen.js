97b46a6efd831ddba330e43d9898be8f4a1575fa
그램 <darkzhon@hanmail.net>
2023-11-11 01:26:59 +0900

    기타 화면 UI 개선

--- alloha/src/screen/EtcScreen.js
+++ alloha/src/screen/EtcScreen.js
@@ -1,7 +1,7 @@
 
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React, {useState, useEffect} from 'react';
-import sampleImage from '../../assets/community.png';
+import { FontAwesome5, MaterialCommunityIcons  } from '@expo/vector-icons';
 
 export default function EtcScreen({navigation}) {
     useEffect(()=>{
@@ -9,23 +9,52 @@ export default function EtcScreen({navigation}) {
     }, [navigation])
 
     return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                onPress={() => navigation.push("storeList")}
-            >
-                <Text>점포관리</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.push("ManageCrew")}
-            >
-                <Text>알바관리</Text>
-            </TouchableOpacity>
-            
+        <View style={[styles.container, {flexDirection:"row"}]}>
+            <GridBox 
+                text={"점포관리"} 
+                onPress={()=>navigation.push("storeList")} 
+                icon={{type:"FontAwesome5", name:"store", size:48, color:"black"}} 
+            />   
+            <GridBox 
+                text={"알바관리"} 
+                onPress={()=>navigation.push("ManageCrew")}
+                icon={{type:"MaterialCommunityIcons", name:"badge-account-horizontal", size:48, color:"black"}}
+            />   
         </View>
     );
 }
 
+function GridBox({text, onPress, icon}){
+    return(
+        <TouchableOpacity onPress={onPress} style={styles.grid}>
+            {
+                (icon)?
+                    (icon.type == "FontAwesome5")?
+                        <FontAwesome5 name={icon.name} size={icon.size} color={icon.color} />
+                    :(icon.type == "MaterialCommunityIcons")?
+                        <MaterialCommunityIcons name={icon.name} size={icon.size} color={icon.color}/>
+                    :
+                        null
+                :
+                    null
+            }
+            <Text style={styles.gridTxt}>{text}</Text>
+        </TouchableOpacity>
+    )
+}
+
 const styles = StyleSheet.create({
     container:{ flex: 1, justifyContent: 'center', alignItems: 'center'},
-    sampleImage:{width:"100%", height:"100%"}
+    grid:{
+        margin:5,
+        padding:20,
+        borderWidth:1,
+        borderColor:"grey",
+        borderRadius:5,
+        alignItems:"center"
+    },
+    gridTxt:{
+        marginTop:10,
+        fontSize:16
+    }
 });
