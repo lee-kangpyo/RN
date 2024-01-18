40def9299aed3a934bf566ff97100e11a40a527d
그램 <darkzhon@hanmail.net>
2024-01-18 23:51:01 +0900

    SCHKNO, ECHKNO -> JOBNO 변경

--- apiServer/src/query/commute.js
+++ apiServer/src/query/commute.js
@@ -7,16 +7,15 @@ const daySchedule = `
     exec PR_PLYA02_ALBASCHMNG @cls, @cstCo, @userId, @ymdFr, @ymdTo, '', '', ''
 `
 const reqCommuteChange = `
-    INSERT INTO PLYADAYJOBREQ (CSTCO, USERID, SCHKNO, ECHKNO, STIME, ETIME, REASON, REQSTAT, USEYN, IUSERID, IYMDHMD)
-    VALUES(@cstCo, @userId, @sChkNo, @eChkNo, @sTime, @eTime, @reason, @reqStat, 'Y', @userId, getdate())
+    INSERT INTO PLYADAYJOBREQ (CSTCO, USERID, JOBNO, STIME, ETIME, REASON, REQSTAT, USEYN, IUSERID, IYMDHMD)
+    VALUES(@cstCo, @userId, @jobNo, @sTime, @eTime, @reason, @reqStat, 'Y', @userId, getdate())
 `
 const initCommuteChange = `
     UPDATE a set a.USEYN = 'N', a.MUSERID=@userId, a.MYMDHMD = getdate()
     from PLYADAYJOBREQ a 
     WHERE CSTCO = @cstCo 
     AND USERID = @userId
-    AND SCHKNO = @sChkNo 
-    AND ECHKNO = @eChkNo
+    AND JOBNO = @jobNo
     AND USEYN = 'Y'
 `
 
