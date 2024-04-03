0b7f8fd75b536c043d7c198ca11bc33690b7d7f1
그램 <darkzhon@hanmail.net>
2024-04-03 16:00:45 +0900

    primary 색상 변경

--- alloha/src/util/color.js
+++ alloha/src/util/color.js
@@ -3,7 +3,7 @@ export const theme = {
     purple:"#7219ed",
     white:"#fff",
     grey:"#C4C4C4",
-    primary:"#375FFE",
+    primary:"#3479EF",
     error:"#EB9AC3",
     correct:"#A9BCE0",
     darkGrey:"#A9A9A9",
