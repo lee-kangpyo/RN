e8aa67f0217f26ebc4308b95a67f82695d5d57c0
그램 <darkzhon@hanmail.net>
2024-04-08 13:18:40 +0900

    eas build 환경 변수 고도화

--- alloha/app.config.js
+++ alloha/app.config.js
@@ -1,10 +1,19 @@
 export default ({ config }) => {
-    //console.log(config);
+    // console.log(config);
+    // console.log(process.env.GOOGLE_SERVICES_JSON);
+    
+    const android = {
+      ...config.android, 
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON, 
+    };
+
+    const ios = {
+      ...config.ios, 
+    }
+
     return {
       ...config,  
-      "android":{
-        "googleServicesFile": process.env.GOOGLE_SERVICES_JSON,
-        "package": "com.akmz.alloha"
-      }
+      "android": android,
+      "ios":ios,
     };
   };
