7ef3a8c6ea9e27fe80d4901f54b510243653061f
그램 <darkzhon@hanmail.net>
2024-05-29 14:49:23 +0900

    알로하 웹에도 접근 가능하게 cors 설정

--- apiServer/server.js
+++ apiServer/server.js
@@ -17,7 +17,7 @@ app.listen(8080, function () {
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://web.aurumforge.com"],
 }));
 
 
