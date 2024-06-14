d4d9043b4fdc4c8c43889b5b2e0661e18f54c156
그램 <darkzhon@hanmail.net>
2024-06-14 20:03:07 +0900

    사용 안하는 종 아이콘 제거 및 ui 조정

--- alloha/src/components/home/HomeHeader.js
+++ alloha/src/components/home/HomeHeader.js
@@ -16,7 +16,7 @@ export default function HomeHeader({data}) {
                     <Text numberOfLines={1} ellipsizeMode="tail" style={[styles.topColor, fonts.font_Bold, {fontSize:20, marginBottom:0,}]}>{data.USERNA}님</Text>
                 </View>
             </View>
-            <Image source={require('../../../assets/icons/alarm.png')} style={{alignSelf:"center", width:24, height:24}} />
+            {/* <Image source={require('../../../assets/icons/alarm.png')} style={{alignSelf:"center", width:24, height:24}} /> */}
         </LinearGradient>
     );
 }
@@ -34,7 +34,7 @@ const fonts = StyleSheet.create({
 const styles = StyleSheet.create({
     container:{flex:1, backgroundColor:"#F6F6F8"},
     top:{paddingTop:60, padding:30, flexDirection:"row", borderBottomLeftRadius:15, borderBottomRightRadius:15, justifyContent:"space-between"},
-    circle:{backgroundColor:"white", borderRadius:50, marginRight:15, width:50, height:50, justifyContent:"center", alignItems:"center"},
+    circle:{backgroundColor:"white", borderRadius:50, marginRight:15, width:50, height:50, paddingTop:5, justifyContent:"center", alignItems:"center"},
     
     topColor:{color:theme.white, },
     body:{padding:20, paddingHorizontal:16},
