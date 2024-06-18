753b1b7d79d409d9a52215461e00ab8e855966cc
그램 <darkzhon@hanmail.net>
2024-06-18 19:46:06 +0900

    바텀 시트 애니메이션 추가

--- alloha/src/components/common/CustomBottomSheet2.js
+++ alloha/src/components/common/CustomBottomSheet2.js
@@ -1,20 +1,45 @@
-import { Dimensions, StyleSheet, View } from "react-native";
-import React from 'react';
+import { Animated, Dimensions, StyleSheet, View } from "react-native";
+import React, { useEffect } from 'react';
+import { useRef } from "react";
 
 const screen = Dimensions.get("screen")
 
 export const CustomBottomSheet2 = ({isOpen, onClose, content}) => {
     const backDrop = (isOpen)?sheet.backDrop:{};
+    const slideAnim = useRef(new Animated.Value(Dimensions.get('window').height)).current; // 애니메이션 초기 값은 화면 높이
+
+    const slideUp = () => {
+        Animated.timing(slideAnim, {
+        toValue: 0, // 최종 값은 0 (원래 위치)
+        duration: 200,
+        useNativeDriver: true,
+        }).start();
+    };
+
+    const slideDown = () => {
+        Animated.timing(slideAnim, {
+        toValue: Dimensions.get('window').height, // 화면 높이로 되돌아가기
+        duration: 200,
+        useNativeDriver: true,
+        }).start();
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+          slideUp();
+        } else {
+          slideDown();
+        }
+      }, [isOpen]);
     return (
         <>
         <View style={backDrop}/>
         {
-            (isOpen)?
-                <View style={sheet.container}>
-                    {content}
-                </View>
-            :
-                null
+            <Animated.View style={[sheet.container, {
+                transform: [{ translateY: slideAnim }],
+                },]}>
+                {content}
+            </Animated.View>
         }
         </>
     )
