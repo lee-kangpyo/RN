e4257ecc761d8211030411dbcddf2c39b4458c91
그램 <darkzhon@hanmail.net>
2024-07-02 19:01:38 +0900

    PR_PLYB02_WRKMNG1 -> PR_PLYB02_WRKMNG 변경

--- apiServer/src/query/dailyReport.js
+++ apiServer/src/query/dailyReport.js
@@ -1,6 +1,6 @@
 
 const DailyReport1 = `
-    exec PR_PLYB02_WRKMNG1 'DailyReport1', @cstCo, '', @ymd, '', '', ''
+    exec PR_PLYB02_WRKMNG 'DailyReport1', @cstCo, '', @ymd, '', '', '', ''
 `
 
 const approve = `
@@ -11,7 +11,7 @@ const jobClose = `
     exec PR_JOB_CLOSE 'jobToSalary', @ymdFr, @ymdTo, @cstCo, '', 'Y'
 `
 const JumjoWorkSave = `
-    exec PR_PLYB02_WRKMNG1 @cls, @cstCo, @useId, @ymd, @sTime, @eTime, @jobCl
+    exec PR_PLYB02_WRKMNG @cls, @cstCo, @useId, @ymd, @sTime, @eTime, @jobCl, @brkDure
 `
 
 const useN_DayJob = `
